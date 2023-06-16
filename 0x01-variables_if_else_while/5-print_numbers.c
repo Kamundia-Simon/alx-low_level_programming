2c1f28d8d09c9e2e357949beacadb6970ea6ec76
Kamundia-Simon <simonkamundia8@gmail.com>
2023-06-16 22:45:11 +0300

    all single digit numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *description - single digit numbers of base 10
+ *Return: (0) always
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 0; x < 10; x++)
+	{
+		printf("%d", x);
+	}
+	printf("\n");
+}
