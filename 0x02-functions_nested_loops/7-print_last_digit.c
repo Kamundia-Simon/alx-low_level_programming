3b993277c6c431b946702d0529a50c7d7f18c749
Kamundia-Simon <simonkamundia8@gmail.com>
2023-06-21 01:20:43 +0300

    prints last digit of integer

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,11 +8,14 @@ int print_last_digit(int x)
 {
 	int a;
 
+	if (x < 0)
+		x = -x;
+
 	a = x % 10;
-	{
-		_putchar(a);
-	}
-		_putchar('\n');
-		return (x);
+	if (a < 0)
+		a = -a;
+	_putchar(a + '0');
+	_putchar('\n');
+	return (x);
 }
 
