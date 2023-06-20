e4dda94e48738c73c742d283115ec8472ff669ae
Kamundia-Simon <simonkamundia8@gmail.com>
2023-06-21 02:06:56 +0300

    prints all natural numbers from n to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - prints numbers upto 89
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int a;
+
+	a = add(89, 9);
+	printf("%d\n", a);
+	return (0);
+}
+
