507870f6a6fd6c75557b6727f1dda469da6c27d2
Kamundia-Simon <simonkamundia8@gmail.com>
2023-06-22 00:24:05 +0300

    sum of even fibonacci less than 4000000

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -6,3 +6,18 @@
   */
 int main(void)
 {
+	int i;
+	long a = 1, b = 2, sum = b;
+
+	while (b + a < 4000000)
+	{
+		b += a;
+		if (b % 2 == 0)
+			sum += b;
+		a = b - a;
+		++i;
+	}
+	printf("\n");
+	return (0);
+}
+
