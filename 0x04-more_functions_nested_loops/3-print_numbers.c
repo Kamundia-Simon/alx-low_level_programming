a1231e18c46326fa402dcd04fcc1e631deccddd3
Kamundia-Simon <simonkamundia8@gmail.com>
2023-06-22 20:22:01 +0300

    prints nmnber 0 to 9 with putchar

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+  *print_number - print numbers 0 to 9
+  */
+void print_number(void)
+{
+	int a;
+
+	for (a = 0; a <= 9; a++)
+	{
+		_putchar(a + 0);
+	}
+	_putchar('\n');
+}
