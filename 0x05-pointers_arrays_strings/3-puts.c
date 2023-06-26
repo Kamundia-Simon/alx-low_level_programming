f5820fee55c197f4c6dc6a2a26de49478596ee48
Kamundia-Simon <simonkamundia8@gmail.com>
2023-06-27 01:17:04 +0300

    prints string followed by new line

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+* _puts - prnts a string followed by anew line to stdout
+*@str: string to be input
+*Retrun: void
+*/
+void _puts(char *str)
+{
+	for (; *str != '\0'; str++)
+	{
+		_putchar(*str);
+	}
+	_putchar('\n');
+}
