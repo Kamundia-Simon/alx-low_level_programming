d835088e7fb804e24203182ed874a60aa905d149
Kamundia-Simon <simonkamundia8@gmail.com>
2023-06-27 00:54:38 +0300

    returns length of string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+  *_strlen - counts length of string
+  *@s: string to be checked
+  *Return: x
+  */
+int _strlen(char *s)
+{
+	int x = 0;
+
+	for (; *s != '\0'; s++)
+	{
+		x++;
+	}
+	return (x);
+}
