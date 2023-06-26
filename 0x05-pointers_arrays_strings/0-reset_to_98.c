a1cc6b68c32d7335a514959a7e2d9fc8b09134d4
Kamundia-Simon <simonkamundia8@gmail.com>
2023-06-26 22:15:48 +0300

    takes a pointet to an int as parametre

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+*reset_to_98 - a fn that takes a pointer to an int as parameter and
+*updates the value it point to 98
+*@n: the value of input n
+*/
+void reset_to_98(int *n)
+{
+	 *n = 98;
+}
