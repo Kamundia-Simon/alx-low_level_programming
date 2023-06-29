0724f2c96fe7ead30456b8faa01831c4a7dfb3d2
Kamundia-Simon <simonkamundia8@gmail.com>
2023-06-29 21:15:34 +0300

    concatenates two strings with limit n bytes

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+  *_strncat - fn to concatenate two strings
+  *@dest: the destination
+  *@src: source value
+  *@n: limit of bytes
+  *Return: destination value
+  */
+char *_strncat(char *dest, char *src, int n)
+{
+	int ln = 0, i = 0;
+
+	while (dest[ln])
+	{
+		ln++;
+	}
+	while (i < n && src[i])
+	{
+		dest[ln] = src[i];
+		ln++;
+		i++;
+	}
+	dest[ln + n + 1] = '\0';
+	return (dest);
+}
