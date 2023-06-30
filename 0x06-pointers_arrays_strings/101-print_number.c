035b7785d64b0fb5e99c5c4fb8502bb2333aea36
Kamundia-Simon <simonkamundia8@gmail.com>
2023-06-30 04:06:15 +0300

    print an integer

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_number - prints an integer
+ * @n:integer print
+ */
+void print_number(int n)
+{
+	{
+		unsigned int m;
+
+		if (n < 0)
+		{
+			m = -n;
+			_putchar('-');
+		} else
+		{
+			m = n;
+		}
+
+		if (m / 10)
+		{
+			print_number(m / 10);
+		}
+
+		_putchar((m % 10) + '0');
+	}
+
+}
