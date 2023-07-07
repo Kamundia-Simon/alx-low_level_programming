d05a461f41b75e56052962f4a4efea1a9f45d7a1
Kamundia-Simon <simonkamundia8@gmail.com>
2023-07-07 21:52:41 +0300

    Creating the static library

--- 0x09-static_libraries/100-atoi.c
+++ 0x09-static_libraries/100-atoi.c
@@ -1,8 +1,10 @@
 #include "main.h"
 /**
  * _atoi - convert a string into an integer.
- * @s: the string
- * Return: int.
+ *
+ * @s: the string to use.
+ *
+ * Return: integer.
  */
 int _atoi(char *s)
 {
