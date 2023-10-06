ec2fddacacc7fb32f3a9db77b4643f9645bb6e29
Kamundia-Simon <simonkamundia8@gmail.com>
2023-10-06 20:52:34 +0300

    get key index

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,12 @@
+#include "hash_tables.h"
+
+/**
+ * key_index - Get index at which a key should be stored
+ * @key: key to get index 
+ * @size: size of array
+ * Return: index of key
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	return (hash_djb2(key) % size);
+}
